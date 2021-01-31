98894c706b557c32bc16c14f32913acce684b682
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 23:45:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@
 def square_array(array)
   number = [1,2,3]
   number.each do |square_number|
-    puts (number.to_i * number.to_i * number.to_i)
+    puts number.to_i * number.to_i * number.to_i
   end
 end
