cb24b0bb41d20fe6fa7936d45d325c5b1c59c30e
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 23:45:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(array)
-  number = [1,2,3]
-  number.each do |square_number|
+  numbers = [1,2,3]
+  numbers.each do |number|
     puts number.to_i * number.to_i * number.to_i
   end
 end
