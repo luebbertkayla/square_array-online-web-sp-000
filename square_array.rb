fb46d649b9725a4df3fe2ccebca228179415eccc
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 23:54:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -8,3 +8,12 @@ def square_array(array)
     return new_array
 end
 
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
+end
