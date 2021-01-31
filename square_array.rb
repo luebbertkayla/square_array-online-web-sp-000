0bf999fcb2ec3e9f1f9840ab59da2322df16d7a0
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 23:55:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
    numbers = [1,2,3]
   numbers.each do |number|
-    new_array = number ** 2
+    new_array << number ** 2
   end
     return new_array
 end
